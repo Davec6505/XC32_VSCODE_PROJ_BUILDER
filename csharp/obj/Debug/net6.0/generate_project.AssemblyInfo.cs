07d78b1a388d83903e0301a12f8e8ba5e1900477
Davec6505 <davec6505@gmail.com>
2025-08-22 13:04:14 +0100

    Project makefile correction and addition of sample working makefile

--- csharp/obj/Debug/net6.0/generate_project.AssemblyInfo.cs
+++ csharp/obj/Debug/net6.0/generate_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("generate_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eb74b5347df1ffb0f2817d9550561ac5c508db6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5b1484d6baa366654afdf82e3d90e364af9f24e")]
 [assembly: System.Reflection.AssemblyProductAttribute("generate_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("generate_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- scripts/generate_projects.bat
+++ scripts/generate_projects.bat
@@ -327,7 +327,7 @@ goto :exiting
 
 
 ::==================================================
-::Shell section
+::batch section
 ::==================================================
 :batch_examples
 setlocal enabledelayedexpansion
@@ -366,39 +366,34 @@ echo.
 set /p choice="Enter your choice (1-5): "
 if "%choice%"=="1" (
     echo Running: generate_project.cmd ExampleBasic
-    bash "%~dp0..\shell\generate_project.sh" . ExampleBasic
+    bash "%~dp0..\cmd\generate_project.cmd" . ExampleBasic
 ) else if "%choice%"=="2" (
     echo Running: generate_project.cmd ExampleDevice 32MZ2048EFH064
-    bash "%~dp0..\shell\generate_project.sh" ExampleDevice . 32MZ2048EFH064
+    bash "%~dp0..\cmd\generate_project.cmd" ExampleDevice . 32MZ2048EFH064
 ) else if "%choice%"=="3" (
-    :project_name_prompt_batch
+    :: Running: python generate_project.py custom name custom directory
+    :project_name_prompt
     set /p PROJECT_NAME="Enter project name: "
     if "!PROJECT_NAME!"=="" (
         echo Project name cannot be empty.
-        goto :project_name_prompt_batch
+        goto :project_name_prompt
     )
-    set /p CUSTOM_DIR="Enter directory (default is C:\Temp\!PROJECT_NAME!): "
-    if "!CUSTOM_DIR!"=="" set "CUSTOM_DIR=C:\Temp\!PROJECT_NAME!"
-    if not exist "!CUSTOM_DIR!" mkdir "!CUSTOM_DIR!"
-    :: Convert Windows path to WSL path for bash (force lowercase drive letter)
-    set "WSL_CUSTOM_DIR=!CUSTOM_DIR:\=/%!"
-    set "WSL_CUSTOM_DIR=/mnt/!WSL_CUSTOM_DIR:~0,1!!WSL_CUSTOM_DIR:~1!"
-    set "WSL_CUSTOM_DIR=!WSL_CUSTOM_DIR:/mnt/C=/mnt/c!"
-    set "SCRIPT_PATH=%~dp0..\shell\generate_project.sh"
-    set "WSL_SCRIPT_PATH=!SCRIPT_PATH:\=/%!"
-    set "WSL_SCRIPT_PATH=/mnt/!WSL_SCRIPT_PATH:~0,1!!WSL_SCRIPT_PATH:~1!"
-    set "WSL_SCRIPT_PATH=!WSL_SCRIPT_PATH:/mnt/C=/mnt/c!"
-    if exist "!SCRIPT_PATH!" (
-        echo Running: generate_project.sh !PROJECT_NAME! 32MZ1024EFH064 !CUSTOM_DIR!
-        bash "!WSL_SCRIPT_PATH!" !PROJECT_NAME! 32MZ1024EFH064 "!WSL_CUSTOM_DIR!"
+    echo ProjectName = !PROJECT_NAME!
+    set /p CUSTOM_DIR="Enter directory (leave blank for C:\Temp\!PROJECT_NAME!): "
+    if "!CUSTOM_DIR!"=="" (
+        echo No directory specified!
+        set "CUSTOM_DIR=C:\Temp\!PROJECT_NAME!"
+        echo Using default directory: !CUSTOM_DIR!
     ) else (
-        echo ERROR: Script not found: !SCRIPT_PATH!
+        echo Directory specified, using !CUSTOM_DIR!.
     )
-    endlocal
-    pause
+    if not exist "!CUSTOM_DIR!" mkdir "!CUSTOM_DIR!"
+    echo Running:  generate_project.cmd !PROJECT_NAME! -o !CUSTOM_DIR!
+    cd /d "%~dp0..\cmd" && generate_project.cmd !PROJECT_NAME! -o !CUSTOM_DIR!
+
 ) else if "%choice%"=="4" (
-    echo Running: generate_project.sh ExampleMikroC 32MZ1024EFH064 C:\Temp mikroc
-    bash "%~dp0..\shell\generate_project.sh" ExampleMikroC 32MZ1024EFH064 C:\Temp mikroc
+    echo Running: generate_project.cmd ExampleMikroC 32MZ1024EFH064 C:\Temp mikroc
+    bash "%~dp0..\cmd\generate_project.cmd" ExampleMikroC 32MZ1024EFH064 C:\Temp mikroc
 ) else if "%choice%"=="5" (
    goto :exiting
 ) else (
@@ -409,6 +404,8 @@ echo Example completed! Check the generated project folder.
 echo.
 echo ==============================================
 
+
 :exiting
+    endlocal
     echo Exiting...
     exit /b
